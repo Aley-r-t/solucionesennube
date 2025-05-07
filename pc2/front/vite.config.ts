5dd94e9ccb9d171a8e9d3cbe4b7963da0bb2989f
aley-r-t <levicom134@gmail.com>
2025-05-06 22:26:37 -0500

    change vite

--- pc2/front/vite.config.ts
+++ pc2/front/vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  // ...otras configuraciones
+
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['unxdcaminando.online']
+  }
+}
